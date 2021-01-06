1d11d28689d525ee2ad874f7fbd64325d39c8065
zoniha <awaawapurin@gmail.com>
2021-01-06 17:43:13 +0900

    prep input data

---
+++ prep_data.cr
@@ -0,0 +1,20 @@
+class PrepData
+  def create_numbers
+    numbers = [] of Int32
+    elements_number = rand(8..10)
+  
+    (0..elements_number).each do
+      numbers << rand(100)
+    end
+    return numbers
+  end
+
+  def show_input_data
+    numbers = create_numbers
+    puts "original"
+    puts numbers
+    puts "sorted"
+    return numbers
+  end
+
+end
