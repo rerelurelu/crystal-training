13c19f6005b8db866d252f4ecaedadc3740db51f
snova <awaawapurin@gmail.com>
2021-03-04 23:31:27 +0900

    fixed bugs

--- prep_data.py
+++ prep_data.py
@@ -1,18 +1,18 @@
 from random import randint
 
-class PrepData():
+class PrepData:
     """
     Create list type data to be
     used in sorting programs.
     """
 
-    def create_numbers:
+    def create_numbers(self):
         elements_number = randint(8, 10)
         numbers = [randint(0, 1000) for _ in range(elements_number)]
         return numbers
 
-    def show_input_data:
-        numbers = create_numbers
+    def show_input_data(self):
+        numbers = self.create_numbers()
         print('original')
         print(numbers)
         print('sorted')
